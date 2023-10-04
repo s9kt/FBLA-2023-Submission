8601faa51d5db033b5517f7055da51956698636b
Pranav Singhal <singhpk@stu.npenn.org>
2023-10-04 15:48:26 -0400

    moved nav styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,3 +4,29 @@
         <li class="nav-item-right"><a href="/login">Log In</a></li>
     </nav>
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  list-style-type: none;
+  flex-wrap: wrap;
+  background-color: #10002B;
+}
+
+nav a {
+  color: #ffffff;
+}
+
+nav a:hover {
+  color:#E0AAFF;
+}
+
+nav li.nav-item-left {
+  float: left;
+  padding: 1em;
+}
+
+nav li.nav-item-right {
+  margin-left: auto;
+}
+</style>
